abfa4a43a0193ca07ff0849024fbf8d35393b64e
bethel bashorun <bashorunbethel99@gmail.com>
2020-09-23 11:21:57 +0100

    final deployment

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const cors = require('cors');
-const fishRoutes = require("./routes/fishes");
+const fishRoutes = require('./routes/fishes');
 const morgan = require('morgan');
-const path = require("path");
-const PORT = process.env.PORT ||  3000;
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 //process.env.NODE_ENV => production or undefined
 
@@ -12,36 +12,38 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(express.json());
-
 
 // app.use(bodyParser.json());
 
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
-app.use("/fishes", fishRoutes )
-
-
-app.use(function(req, res, next) {
-    const err = new Error();
-    error.status = 404;
-    next(err)
+app.use('/fishes', fishRoutes);
+app.get('/home', function (req, res) {
+  res.send('Welcome');
 });
 
-if(app.get("env") === "development"){
-    app.use((err, req, res, next)=> {
-        res.status(err.status || 500)
-        res.send({
-            message: err.message,
-            error: err
-        });
+app.use(function (req, res, next) {
+  const err = new Error();
+  error.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: err,
     });
+  });
 }
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join(__dirname,"client/fishes_app/build")))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/fishes_app/build')));
 }
 console.log('__dirname', __dirname);
 
-app.listen(PORT, () => {console.log(`server started port 3000 ${PORT}`)}); 
+app.listen(PORT, () => {
+  console.log(`server started port 3000 ${PORT}`);
+});
